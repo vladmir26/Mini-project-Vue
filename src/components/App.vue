0c5c356a681fe5ce335bf764c341c6b68c0c1d9d
vladmir26 <97904721+vladmir26@users.noreply.github.com>
2023-07-06 18:12:10 +0300

    Started making a mini-project with Vue

---
+++ src/components/App.vue
@@ -0,0 +1,48 @@
+<script>
+
+</script>
+
+<template>
+  <div class="converter container">
+  <div class="converter__inner-wrapper">
+      <label>Degrees</label>
+      <input type="number">
+      <label>Type</label>
+      <select>
+        <option>
+          Fahrenheit
+        </option>
+        <option>
+          Celsius
+        </option>
+      </select>
+      <button type="button">Convert</button>
+      <label>Result</label>
+      <input type="number">
+  </div>
+  </div>
+</template>
+
+<style lang="scss">
+body {
+  background-color: lightgrey;
+}
+
+.container {
+  max-width: 1440px;
+  margin-right: auto;
+  margin-left: auto;
+}
+.converter {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  &__inner-wrapper {
+    width: 500px;
+    height: 200px;
+    background-color: white;
+}
+
+} 
+</style>
---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import { fileURLToPath, URL } from 'node:url'
+
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import VitePluginBrowserSync from 'vite-plugin-browser-sync'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    VitePluginBrowserSync()
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
